d3f4e19576b7daa9a0b315d26da58ff3fcecf28a
hgjohn <hjohnston.g@gmail.com>
2023-07-24 16:10:35 -0700

    quart server for bot commands

--- server.py
+++ server.py
@@ -1,26 +1,29 @@
 from quart import Quart, render_template, redirect, url_for, request, jsonify
 from quart_discord import DiscordOAuth2Session
+from dotenv import dotenv_values
 import os
 from gptfunctions import chat
 
-os.environ[
-    "OAUTHLIB_INSECURE_TRANSPORT"
-] = "1"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+config = dotenv_values(".env")
 
 app = Quart(__name__)
+app.secret_key = config["SECRET-KEY"]
 
-
-app.config["SECRET_KEY"] = "test123"
-
-app.config["DISCORD_CLIENT_ID"] = 969522874446671953  # Discord client ID.
-app.config[
-    "DISCORD_CLIENT_SECRET"
-] = "dfp9GSgUHqvIMBSEIsrG9DW1XMnJskhl"  # Discord client secret.
-app.config[
-    "DISCORD_REDIRECT_URI"
-] = "http://127.0.0.1:5000/callback"  # URL to your callback endpoint.
+app.config["DISCORD_CLIENT_ID"] = config["DISCORD-CLIENTID"]  # Discord client ID.
+app.config["DISCORD_CLIENT_SECRET"] = config["DISCORD-CLIENTSECRET"]  # Discord client secret.
+app.config["DISCORD_REDIRECT_URI"] = config["DISCORD-REDIRECT-URI"]  # URL to your callback endpoint.
 
 discord = DiscordOAuth2Session(app) #handle session for authentication
+
+@app.route("/")
+async def home():
+    return await render_template("home.html", authorized=await discord.authorized)
+
+@app.route("/login")
+async def login():
+    return await discord.create_session() # handles session creation for authentication
 
 @app.route('/gpt', methods=['POST'])
 async def gpt():
