9bb450b47663666661ed1d9136d7fe6e2e0dfe36
hgjohn <hjohnston.g@gmail.com>
2023-07-31 02:54:35 -0700

    generate requirements

--- gptfunctions.py
+++ gptfunctions.py
@@ -8,7 +8,7 @@ def chat(prompt: str) -> str:
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are an AI assisstant."},
             {"role": "user", "content": prompt}
         ]
     )
