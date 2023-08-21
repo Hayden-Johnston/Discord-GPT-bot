7f0f7257a1b6cee6bc44a58ee82ad15e2a1f6f1b
Hayden-Johnston <hjohnston.g@gmail.com>
2023-08-20 18:09:58 -0700

    start of sqlite db

---
+++ db.py
@@ -0,0 +1,10 @@
+# author: Hayden Johnston
+# date: 08/20/2023
+# description: SQLite implementation for GPT-3 chat memory
+
+import sqlite3, os
+
+# on chat command, get user idea, check user chat memory
+    # if user chat memory exists, append user idea to user chat memory
+    # if user chat memory does not exist, create user chat memory and append user idea to user chat memory
+# 
