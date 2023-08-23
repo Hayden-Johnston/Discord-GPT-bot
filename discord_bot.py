57d2184cd6c5e8949f1f8eb4f4fd96b52bcf237a
Hayden-Johnston <hjohnston.g@gmail.com>
2023-08-22 20:43:21 -0700

    added run function

--- discord_bot.py
+++ discord_bot.py
@@ -22,16 +22,13 @@ async def gpt(ctx):
     """Check channel ID and send message to channel"""
     channel = ctx.channel
     content = ctx.message.content
+    user_id = ctx.message.author.id
     if channel.id == 1129457187962499163:
         response = chat(content)
         await channel.send(response)
 
-# ---------------------------- FUNCTIONS ------------------------------- #
-
-
-    
-# ------------------------------- RUN ---------------------------------- #
+# ----------------------------- FUNCTIONS ------------------------------ #
 
 def run():
-    """Run bot"""
+    """Start function"""
     bot.run(discord_token)
