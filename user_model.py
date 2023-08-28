edd7dd7d4079f920248f0357b62e7dabbda62fab
Hayden-Johnston <hjohnston.g@gmail.com>
2023-08-27 23:45:16 -0700

    user class

--- user_model.py
+++ user_model.py
@@ -5,8 +5,30 @@
 class User:
     """Define User object to maintain chat memory"""
 
-    def __init__(self, id, memory):
+    def __init__(self, id):
         self.depth = 2
         self.id = id
-        self.memory = memory
+        self.memory = []
+
+    def change_depth(self, depth):
+        """Change user chat memory depth"""
+        self.depth = depth
+        return None
         
+    def remember(self):
+        """Remember user chat memory"""
+        return self.memory
+    
+    def forget(self):
+        """Forget user chat memory"""
+        self.memory = []
+        return None
+    
+    def update(self, content):
+        """Update user chat memory"""
+        if len(self.memory) >= self.depth:
+            self.memory.pop(0)
+            self.memory.append(content)
+        else:
+            self.memory.append(content)
+        return None
