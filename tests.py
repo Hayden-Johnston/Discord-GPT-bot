54aced25af86e060fe2a70bd1042a8c46160df89
Hayden-Johnston <hjohnston.g@gmail.com>
2023-08-30 21:34:25 -0700

    unit tests for db functions

--- tests.py
+++ tests.py
@@ -1,11 +1,41 @@
 import unittest
-import db
+from db import insert_memory, update_memory, delete_memory, get_by_id, get_all, create_table
 import app
 
 class TestBot(unittest.TestCase):
 
-    def test_create_memory(self):
-        """Test create_memory function"""
-        data = {"user_id": 1, "memory": ["test"]}
-        db.insert_memory(data)
-        self.assertEqual(db.get_by_id(1)['memory'], ["test"])
+    def test_create_table(self):
+        """Create table for testing"""
+        create_table()
+        return None
+
+    def test_insert_memory(self):
+        """Test insert_memory function"""
+        cases = [
+            {"user_id": 1, "memory": ["testing database"]},
+            {"user_id": 2, "memory": ["hello world"]}
+        ]
+        for case in cases:
+            insert_memory(case)
+            row = get_by_id(case['user_id'])
+            self.assertEqual(get_by_id(case['user_id'])['memory'], case['memory'])
+
+    def test_update_memory(self):
+        """Test update_memory function"""
+        cases = [
+            {"user_id": 1, "memory": ["testing SQLite"]},
+            {"user_id": 2, "memory": ["hello discord"]}
+        ]
+        for case in cases:
+            update_memory(case)
+            self.assertEqual(get_by_id(case['user_id'])['memory'], case['memory'])
+
+    def delete_memory(self):
+        """Test delete_memory function"""
+        cases = [ 1, 2]
+        for case in cases:
+            delete_memory(case)
+            self.assertEqual(get_by_id(case), None)
+
+if __name__ == '__main__':
+    unittest.main()
