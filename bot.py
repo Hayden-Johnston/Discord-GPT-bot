72e34d421793f9919a3c659c24ab19a0940b5691
Hayden-Johnston <hjohnston.g@gmail.com>
2023-09-08 18:54:51 -0700

    gpt memory working now

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 # author: Hayden Johnston
-# date: 08/20/2023
+# date: 09/07/2023
 # description: Discord bot for GPT-3 chatbot
 
 import discord, os
 from discord.ext import commands
 from dotenv import load_dotenv
 from app import chat, handle_memory
-from db import get_by_id, insert_memory, update_memory, delete_memory
+from db import get_by_id, delete_memory
 
 if os.path.exists(".env") == True:
     load_dotenv()
@@ -17,7 +17,7 @@ intents.message_content = True
 bot=commands.Bot(command_prefix="/", intents=intents)
 
 # ----------------------------- COMMANDS ------------------------------- #
-
+# data saved as string separated by ", " - [{"role": "system", "content": "You are an AI assisstant that only responds in 400 or less characters without commas."}, {"role": "user", "content": "Hello World"}}]
 @bot.command()
 async def gpt(ctx):
     """Query GPT-3 from chat-gpt channel"""
@@ -25,19 +25,31 @@ async def gpt(ctx):
     content = ctx.message.content
     user_id = ctx.message.author.id
     if channel.id == 1129457187962499163:
-        
-        handle_memory(user_id, content[5:])
-        user_memory = get_by_id(user_id)[0][1]
-
-        data = [
-            {"role": "system", "content": "You are an AI assisstant that only responds in 400 or less characters without commas."},
-            {"role": "user", "content": content[5:]}
-        ]
-
         if len(content) > 400:
             response = "Error: character limit (400) exceeded."
         else:
-            response = chat(data)
+            user_memory = get_by_id(user_id)
+
+            if user_memory == []:
+                data = [
+                    {"role": "system", "content": "You are an AI assisstant that only responds in 400 or less characters without commas."},
+                    {"role": "user", "content": content[5:]}
+                ]
+                response = chat(data)
+                handle_memory(user_id, content[5:], response)
+
+            else:
+                data = [
+                    {"role": "system", "content": "You are an AI assisstant that only responds in 400 or less characters without commas."},
+                    {"role": "user", "content": user_memory[0][1][0]},
+                    {"role": "system", "content": user_memory[0][1][1]},
+                    {"role": "user", "content": user_memory[0][1][2]},
+                    {"role": "system", "content": user_memory[0][1][3]},
+                    {"role": "user", "content": content[5:]}
+                ]
+                response = chat(data)
+                handle_memory(user_id, content[5:], response)
+
         await channel.send(response)
 
 @bot.command()
@@ -46,7 +58,6 @@ async def dgpt(ctx):
     channel = ctx.channel
     content = ctx.message.content
     if channel.id == 1129457187962499163:
-
         data = [
             {"role": "system", "content": "You are an AI assisstant that only responds in 2000 or less characters."},
             {"role": "user", "content": content[6:]}
